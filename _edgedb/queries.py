72b3e0d328b742bbdfc358bf103d1f838f59b417
Mahmoud Hussein <mahmoudh3257@gmail.com>
2023-05-18 22:44:52 +0200

    Add more queries

--- _edgedb/queries.py
+++ _edgedb/queries.py
@@ -8,3 +8,55 @@ GET_ANSWER = """
         is_accepted
     } FILTER .id = <uuid>$id
 """
+
+GET_COMMENTS_ON_QUESTION = """
+    WITH comments_ids := (SELECT Question FILTER .id = <uuid>$id).comments.id
+    SELECT Comment{
+    content,
+    upvote,
+    downvote,
+    author
+    } FILTER .id IN comments_ids
+"""
+
+INSERT_USER = """
+    SELECT (
+        INSERT User {
+            age := <Age>$age,
+            email := <str>$email,
+            first_name := <str>$fname,
+            last_name := <str>$lname,
+            username := <str>$username,
+            is_active := <bool>$is_active,
+            is_superuser := <bool>$is_superuser,
+            is_verified := <bool>$is_verified,
+            hashed_password := <str>$hashed_password,
+        }
+    ) {
+        id,
+        username,
+        email,
+        age,
+        first_name, 
+        last_name,
+    }
+"""
+
+UPDATE_COMMENTS_ON_ANSWER = """
+    SELECT (
+        UPDATE Answer FILTER .id = <uuid>$answer_id
+        SET {
+            comments += (
+                INSERT Comment {
+                    upvote := <int16>$upvote,
+                    downvote := <int16>$downvote,
+                    content := <str>$content,
+                    author := (SELECT User FILTER .id = <uuid>$author_id)
+                }
+            )
+        }
+    ) {
+        id,
+        comments,
+    }
+"""
